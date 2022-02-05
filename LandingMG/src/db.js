2704eaf9ccd93fe240520f0fb0d5144c705eca4e
yas lillo <yasnalillo27@gmail.com>
2022-02-05 20:23:58 -0300

    carpetas para deplo

--- LandingMG/src/db.js
+++ LandingMG/src/db.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(`${DATABASE_URL}`, {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 
 });
 
